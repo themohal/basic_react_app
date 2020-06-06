7ae22033d9ec5b872707d8aa647ecd0ed737948a
themohal <farjadmohal@gmail.com>
2020-06-06 20:42:52 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <br>Update By Muhammad Farjad Ali Raza</br>
+        Edit <code>src/App.js</code> and save to reload. 
+          <br/>
+          Update By Muhammad Farjad Ali Raza
         </p>
         <a
           className="App-link"
